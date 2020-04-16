161e7eafdb042b1346276bad6b7312410d1008b6
Enzo Maruffa <enzomaruffa@gmail.com>
2020-04-16 15:03:20 -0300

    Adds Users migration, adds hash dependency

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :reflections, Reflections.Repo,
 # with webpack to recompile .js and .css sources.
 config :reflections, ReflectionsWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
